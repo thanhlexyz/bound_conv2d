4efc0a123eb2966c39c3a402883edc915837d27c
Thanh Le <hi@thanhle.xyz>
2026-04-10 17:19:20 +0900

    updates

--- conv2d_wedge.py
+++ conv2d_wedge.py
@@ -15,35 +15,42 @@ class Conv2dWedge:
     def __str__(self):
         return f'Conv2dWedge(shape={self.shape})'
 
+    @property
+    def shape(self):
+        return self.W_L.shape
+
     def accumulate_weight(self, weight, bias, *, attr=None):
         # TODO
         return type(self)(new_W_L, new_b_L, new_W_U, new_b_U)
 
-    def to_bound_tensor(self, x):
-        # x: BoundTensor, input bound tensor
-        # return output bound tensor
-        # TODO, not done yet
-        x = x[0]
-        x_L, x_U = x.concretize()
-        x_mean = 0.5 * (x_U - x_L)
-        x_std  = 0.5 * (x_U - x_L)
-        # extract dimension
-        bs, c_in, h_in, w_in = x.shape
-        _, c_out, h_out, w_out, _, k_h, k_w = self.shape
-        # BEGIN: not done yet
-        L = torch.empty(bs, c_out, h_out, w_out)
-        U = torch.empty(bs, c_out, h_out, w_out)
-        def concretize_one_side(W, b, sign):
-            for i in range(bs):
-                for h in range(h_out):
-                    for w in range(w_out):
-                        temp = (x_mean[bs, ...] * self.W_L[i, :, h, w, ...])
-                        L[i, :, h, w] = temp + self.b_L
-            return bound
-        # END: not done yet
-        L = concretize_one_side(self.W_L, self.b_L, -1)
-        U = concretize_one_side(self.W_U, self.b_U, 1)
-        # return a BoundTensor of correct type
-        p = type(x.perturbation)(L, U)
-        z = type(x)(L, p)
-        return z
+#     def to_bound_tensor(self, F_conv, x):
+#         # x: BoundTensor, input bound tensor
+#         # return output bound tensor
+#         # TODO, not done yet
+#         x = x[0]
+#         x_L, x_U = x.concretize()
+#         x_mean = 0.5 * (x_U - x_L)
+#         x_std  = 0.5 * (x_U - x_L)
+#         # extract dimension
+#         bs, c_in, h_in, w_in = x.shape
+#         _, c_out, h_out, w_out, _, k_h, k_w = self.shape
+#         # BEGIN: not done yet
+#         L = torch.empty(bs, c_out, h_out, w_out)
+#         U = torch.empty(bs, c_out, h_out, w_out)
+#         def concretize_one_side(W, b, sign):
+#             for i in range(bs):
+#                 for h in range(h_out):
+#                     for w in range(w_out):
+#                         temp = (x_mean[bs, ...] * self.W_L[i, :, h, w, ...])
+#                         L[i, :, h, w] = temp + self.b_L
+#             return bound
+#         # END: not done yet
+#         L = concretize_one_side(self.W_L, self.b_L, -1)
+#         U = concretize_one_side(self.W_U, self.b_U, 1)
+#         # return a BoundTensor of correct type
+#         p = type(x.perturbation)(L, U)
+#         z = type(x)(L, p)
+#         return z
+
+    def to_bound_tensor(self, F_conv, x):
+        pass # <--- my immediate task
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torch
 
 if __name__ == '__main__':
     # import
+    from conv2d_wedge import Conv2dWedge
     from bound_tensor import BoundTensor
     from bound_linf import BoundLinf
 
@@ -12,6 +13,7 @@ if __name__ == '__main__':
     U = L + 0.02
     p = BoundLinf(L, U)
     x = BoundTensor(L, p)
+
     # initialize weight and bias for conv2d
     weight = torch.randn(2, 3, 5, 5)
     bias = torch.randn(2)
@@ -19,22 +21,30 @@ if __name__ == '__main__':
     # forward
     x0 = x.sample()
     y0 = F.conv2d(x0, weight, bias=bias, stride=1, padding=1, dilation=1, groups=1)
-    print(y0.shape)
+    # print(y0.shape)
 
     # identity conv: F.conv2d(y0, W_L, b_L) == y0 (same shape, stride=1, padding=0)
     C = y0.shape[1]
     W_L = torch.zeros(C, C, 1, 1, dtype=y0.dtype, device=y0.device)
     W_L[torch.arange(C, device=y0.device), torch.arange(C, device=y0.device), 0, 0] = 1.0
     b_L = torch.zeros(C, dtype=y0.dtype, device=y0.device)
+    W_U = W_L.clone()
+    b_U = b_L.clone()
+    wedge = Conv2dWedge(W_L, b_L, W_U, b_U)
+    # print(wedge)
 
+    # test if input for wedge is actually identity
     y1 = F.conv2d(y0, W_L, bias=b_L, stride=1, padding=0)
     same = torch.allclose(y1, y0)
     err = (y1 - y0).abs().max().item()
-    print(f'[y1 vs y0] {same=} {err=}')
+    # print(f'[y1 vs y0] {same=} {err=}')
 
-#
-#     print(y.shape)
-#     print(
-#         torch.allclose(y, y_hat, rtol=1e-4, atol=1e-5),
-#         (y - y_hat).abs().max().item(),
-#     )
+    # initialize y as bound tensor
+    L = torch.randn_like(y0)
+    U = L + 0.02
+    p = BoundLinf(L, U)
+    y = BoundTensor(L, p)
+    print(y)
+
+    # realize bound tensor for y using identity conv2d wedge
+    # identical_y = wedge.to_bound_tensor(F_conv, y)
