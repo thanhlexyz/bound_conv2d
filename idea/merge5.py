e40fa8f31fa2a8ada26639f49ee7d9882903f6fa
Thanh Le <hi@thanhle.xyz>
2026-04-19 19:03:09 +0900

    x

---
+++ idea/merge5.py
@@ -0,0 +1,30 @@
+import torch.nn.functional as F
+import numpy as np
+import torch
+
+# setup
+# 2, 3, 3, 3 -> 3, 5, 2, 5
+kernel_size_1 = np.array([3, 3])
+kernel_size_2 = np.array([3, 5])
+weight1 = torch.randn(2, 3, 3, 3)
+weight2 = torch.randn(3, 5, 3, 5)
+bias1 = torch.randn(2)
+bias2 = torch.randn(3)
+
+# merge
+kernel_size_12 = kernel_size_1 + kernel_size_2 - 1
+padding = [kernel_size_2[0] - 1, kernel_size_2[1] - 1]
+weight12 = F.conv2d(weight1.data.permute(1, 0, 2, 3),
+                    weight2.data.flip(-1, -2),
+                    padding=padding).permute(1, 0, 2, 3)
+_ = torch.ones(1, 3, *kernel_size_2) * bias1[None, :, None, None]
+bias12 = F.conv2d(_, weight2, bias2).flatten()
+# verify output
+x = torch.randn(1, 2, 9, 9)
+y = F.conv2d(F.conv2d(x, weight1, bias1), weight2, bias2)
+y_hat = F.conv2d(x, weight12, bias12)
+
+print(y)
+print(y_hat)
+
+assert torch.allclose(y, y_hat)
